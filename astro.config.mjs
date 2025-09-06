4054da2647e0164c8f6b3fe607a522c054848ac5
Hoomexsun Pangsatabam <humex.pangz@gmail.com>
2025-09-06 21:55:16 +0530

    Added CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hoomexsun.github.io',
-    base: '/ehei',
-    // site: 'https://ehei.in',
+    // site: 'https://hoomexsun.github.io',
+    // base: '/ehei',
+    site: 'https://ehei.in',
 });
